8fb7fb37c86b1a8d243fd5eeb1cb90fe389c2432
ajsmack <38812720+ajsmack@users.noreply.github.com>
2021-09-23 11:11:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import joblib
+from flask import Flask , render_template , request
+
+
+
+app = Flask(__name__)
+
+
+model = joblib.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+
+def hello():
+    return render_template('index.html')
+
+@app.route('/',methods=['GET','POST'])
+
+def predict():
+  input_values = [float(x) for x in request.form.values()]
+  inp_features = [input_values]
+  prediction = model.predict(inp_features)
+  return render_template('index.html',output='The estimated cost of insurance is {} $'.format(int(prediction)))
+  
+if __name__=='__main__':
+  app.run(debug=True)
